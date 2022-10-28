9e2547e2347ff7ee776ada6c4eca3850db254d81
Caíque Santos <caique_pedro@hotmail.com>
2022-10-28 07:52:46 -0300

    finished node with nginx

---
+++ desafio-node-nginx/node/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database:'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+app.get('/', (req,res) => {
+  const insertQuery = `INSERT INTO people(name) values('Caíque')`
+  connection.query(insertQuery)
+
+  const sql = `SELECT * FROM people`
+  connection.query(sql, (error, results) => {
+    if (error) {
+      return console.error(error.message);
+    }
+
+    res.send('<h1>Full Cycle</h1>' + results.map((result) => result.name).join(', '))
+  })
+})
+
+app.listen(port, ()=> {
+  console.log('Rodando na porta ' + port)
+})
